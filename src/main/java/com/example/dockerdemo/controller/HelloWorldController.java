872570e886872dd8d0135a7c8654cbe93506df06
samyuktha215 <samyuktha.basam@gmail.com>
2023-05-31 10:11:55 +0200

    changes in controller class

--- src/main/java/com/example/dockerdemo/controller/HelloWorldController.java
+++ src/main/java/com/example/dockerdemo/controller/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HelloWorldController {
-    @GetMapping("/HelloWorld!")
+    @GetMapping("/HelloWorld!!!!!!!")
 public String getStringHello(){
         return "HelloWorld";
 
